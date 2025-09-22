0cd53907f615d4dca316d9316ffc7d668ae561db
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2025-09-22 17:41:55 +0100

    Use git for module sources

--- local/apps/main.tf
+++ local/apps/main.tf
@@ -1,33 +1,33 @@
 module "tailscale" {
-  source = "../../../modules/apps/tailscale"
+  source = "git::https://github.com/darigan-local/modules.git//apps/tailscale"
   model = var.tailscale
   oauthClientId = var.TAILSCALE_OAUTH_CLIENT_ID
   oauthClientSecret = var.TAILSCALE_OAUTH_CLIENT_SECRET
 }
 
 module "jellyfin" {
-  source = "../../../modules/apps/servarr"
+  source = "git::https://github.com/darigan-local/modules.git//apps/servarr"
   model = var.jellyfin
 
   depends_on = [module.tailscale]
 }
 
 module "prowlarr" {
-  source = "../../../modules/apps/servarr"
+  source = "git::https://github.com/darigan-local/modules.git//apps/servarr"
   model = var.prowlarr
 
   depends_on = [module.tailscale]
 }
 
 module "sonarr" {
-  source = "../../../modules/apps/servarr"
+  source = "git::https://github.com/darigan-local/modules.git//apps/servarr"
   model = var.sonarr
 
   depends_on = [module.tailscale]
 }
 
 module "transmission" {
-  source = "../../../modules/apps/servarr"
+  source = "git::https://github.com/darigan-local/modules.git//apps/servarr"
   model = var.transmission
 
   depends_on = [module.tailscale]
--- local/storage/main.tf
+++ local/storage/main.tf
@@ -5,33 +5,33 @@ resource "kubernetes_namespace_v1" "servarr" {
 }
 
 module "storage-class" {
-  source = "../../../modules/storage/storage_class"
+  source = "git::https://github.com/darigan-local/modules.git//storage/storage_class"
   model = var.storage-class
 }
 
 module "sonarr-config-pvc" {
-  source = "../../../modules/storage/persistent_volume_claim"
+  source = "git::https://github.com/darigan-local/modules.git//storage/persistent_volume_claim"
   model = var.sonarr-config-pvc
 
   depends_on = [module.storage-class]
 }
 
 module "prowlarr-config-pvc" {
-  source = "../../../modules/storage/persistent_volume_claim"
+  source = "git::https://github.com/darigan-local/modules.git//storage/persistent_volume_claim"
   model = var.prowlarr-config-pvc
 
   depends_on = [module.storage-class]
 }
 
 module "transmission-config-pvc" {
-  source = "../../../modules/storage/persistent_volume_claim"
+  source = "git::https://github.com/darigan-local/modules.git//storage/persistent_volume_claim"
   model = var.transmission-config-pvc
 
   depends_on = [module.storage-class]
 }
 
 module "jellyfin-config-pvc" {
-  source = "../../../modules/storage/persistent_volume_claim"
+  source = "git::https://github.com/darigan-local/modules.git//storage/persistent_volume_claim"
   model = var.jellyfin-config-pvc
 
   depends_on = [module.storage-class]
